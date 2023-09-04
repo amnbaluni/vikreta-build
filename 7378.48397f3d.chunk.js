8e1d77ee30331d1a5ca78d0ae33942cde8e4a193
Aman <amanbaluni.abab@gmail.com>
2023-09-04 23:02:27 +0530

    first commit

---
+++ 7378.48397f3d.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapi=self.webpackChunkapi||[]).push([[7378],{17378:(a,t,n)=>{n.r(t),n.d(t,{AUTH_ROUTES_EE:()=>h});var s=n(67294);const h=[{component:s.lazy(()=>n.e(4544).then(n.bind(n,84544)).then(E=>({default:E.AuthResponse}))),path:"/auth/login/:authResponse"}]}}]);
